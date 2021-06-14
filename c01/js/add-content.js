19689d8cf36236ae88cf89053910c1bf014b5eb0
fabricio <fabricio.jacob@outlook.com>
2021-06-13 22:04:33 -0300

    add

--- c01/js/add-content.js
+++ c01/js/add-content.js
@@ -3,13 +3,11 @@ var hourNow = today.getHours();
 var greeting;
 
 if (hourNow > 18) {
-    greeting = 'Good evening!';
-} else if (hourNow > 12) {
+    greeting = 'Goog evening!';
+} else  if (hourNow > 12) {
     greeting = 'Good afternoon!';
 } else if (hourNow > 0) {
-    greeting = 'Good morning!';
-} else {
     greeting = 'Welcome!';
 }
 
-document.write('<h3>' + greeting + '</h3>');
+document.write('<h3>' + greeting + '</h3>')
